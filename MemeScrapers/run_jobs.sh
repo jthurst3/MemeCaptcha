a98c84526aaa362abb78f4e4447ff75c56c85dc2
Hassler Thurston <jthurst3@u.rochester.edu>
2017-04-16 21:13:33 -0400

    modify run_jobs for memegenerator

--- MemeScrapers/run_jobs.sh
+++ MemeScrapers/run_jobs.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -i
 
 computers="blake cascade colden macomb marcy nye phelps porter sawteeth seward tabletop algonquin armstrong cliff colvin gothics haystack hough iroquois marshall panther redfield skylight whiteface wright"
-lower=10000000
+lower=35000000
 size=1000000
 
 for c in $computers; do
 	upper=$(($lower + size))
-	echo $lower $upper $c
+	echo $c $lower $upper
 
 	ssh -t $c "cd Courses/298/MemeCaptcha/MemeScrapers; ./scrape.sh $lower $upper $c" < /dev/tty
+	if [ $? -eq 0 ]; then
+		echo "$c $lower $upper" >> current_run.txt
+	else
+		echo "$c $lower $upper FAILED TO START" >> current_run.txt
+	fi
 
 	lower=$(($lower + size))
 done
