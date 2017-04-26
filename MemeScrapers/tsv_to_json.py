9be2159d2a385cb60f8b20a7a29169112d748ce2
Hassler Thurston <jthurst3@u.rochester.edu>
2017-04-26 02:09:58 -0400

    update tsv_to_json script

--- MemeScrapers/tsv_to_json.py
+++ MemeScrapers/tsv_to_json.py
@@ -11,18 +11,20 @@ import json
 import os
 import sys
 
-directory = '/public/jthurst3/MemeCaptcha/data'
-output_file = '/public/jthurst3/MemeCaptcha/cleaned_data/first_100.json'
+directory = '/public/jthurst3/MemeCaptcha/data/sharded'
+output_file = '/public/jthurst3/MemeCaptcha/cleaned_data/memes.json'
 
-special = 'memegenerator_first_100.tsv'
+bad_files = ['memegenerator_first_100.tsv',
+'memegenerator_armstrong_22000000_23000000aa',
+'memegenerator_marshall_29000000_30000000ai',
+]
+# special = 'memegenerator_armstrong_22000000_23000000.tsv'
 
 # gets a set of all TSV files in a directory, excluding the special one
 def get_files(direc):
 	files = os.listdir(direc)
-	tsv = set([f for f in files if f.endswith('.tsv')])
-	if special in tsv:
-		tsv.remove(special)
-	return tsv
+	tsv = set(files)
+	return tsv.difference(bad_files)
 
 # gets an image ID from its memegenerator URL
 def get_id(url):
@@ -46,14 +48,24 @@ def bottom_text(t):
 # assumes f is a file path, so we need to open it.
 def append_entries(j, path):
 	memes = j['memes']
-	f = open(path, 'r')
+	f = open(path, 'rU')
 	reader = csv.reader(f, delimiter='\t')
+	counter = 0
+	badcounter = 0
 	for row in reader:
+		counter += 1
+		if len(row) < 3:
+			# print("ROW SMALL", row)
+			badcounter += 1
+			continue
 		jo = {}
 		jo['image_id'] = get_id(row[0])
 		jo['top_text'] = top_text(row[1])
 		jo['bottom_text'] = bottom_text(row[2])
 		memes.append(jo)
+	print("finished appending from ", path, "bad entries", badcounter,
+			"total entries", counter, "success",
+			float(counter-badcounter)/counter)
 	return j
 
 # main method: create JSON file
@@ -66,10 +78,11 @@ def create_JSON():
 	out = open(output_file, 'w')
 	j = {}
 	j['memes'] = []
-	# files = get_files(directory)
+	files = [os.path.join(directory, f) for f in get_files(directory)]
 	# testing
-	files = [os.path.join(directory, special)]
+	files = [os.path.join(directory, 'memegenerator_marshall_29000000_30000000aa')]
 	for f in files:
+		print("starting from ", f)
 		append_entries(j, f)
 	json.dump(j, out)
 	
